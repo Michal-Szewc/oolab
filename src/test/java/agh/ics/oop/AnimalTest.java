5b5e7cb664da3e649ec97825081cef5956c16e27
MichalSzewc <szewc18michal@gmail.com>
2021-11-04 01:46:59 +0100

    lab3

--- src/test/java/agh/ics/oop/AnimalTest.java
+++ src/test/java/agh/ics/oop/AnimalTest.java
@@ -12,10 +12,14 @@ class AnimalTest {
             animal.move(move);
         assertTrue(animal.isAt(new Vector2d(1,4)));
         assertEquals(animal.getDirection(),MapDirection.NORTH);
-        for(MoveDirection move: OptionsParser.parse(new String[]{"f","f","f","r","f","f","f","f","f","f","r","b","b","b","b","b","b","b"})) {
+        for(MoveDirection move: OptionsParser.parse(new String[]{"f","f","f","r","f","f","f","f","f","f","r","f","f","f","f","f","f","f"})) {
             animal.move(move);
             assertTrue(animal.getPosition().proceeds(new Vector2d(4, 4)));
             assertTrue(animal.getPosition().follows(new Vector2d(0, 0)));
         }
+        assertTrue(animal.isAt(new Vector2d(4,0)));
+        for(MoveDirection move: OptionsParser.parse(new String[]{"forwad","frowad","frawod","frowda","forwadd","hogward","prosze rusz sie zwierzaczku","dlaczego to nie dziala","inne glupie teksty"}))
+            animal.move(move);
+        assertTrue(animal.isAt(new Vector2d(4,0)));
     }
 }
